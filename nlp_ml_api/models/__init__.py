aecb4ce16eebb88d78ec5981c2df105c50ba20f6
patrickjohncyh <patrickjohncyh@icloud.com>
2022-09-07 19:38:12 -0400

    remove sbert

--- nlp_ml_api/models/__init__.py
+++ nlp_ml_api/models/__init__.py
@@ -1,3 +1,2 @@
 from nlp_ml_api.models.CountBasedModel import CountModel, TFIDFModel, CountModelParams, TFIDFModelParams
-from nlp_ml_api.models.SBertModel import SBertModel
 from nlp_ml_api.models.TransformerModel import TransformerModel, TransformerModelParams
