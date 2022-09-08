9f6d00f8bcbd44c98f4e3767f257005243869310
patrickjohncyh <patrickjohncyh@icloud.com>
2022-09-08 17:56:12 -0400

    Update makefile; add heroku deployment to deploy function

--- nlp_ml_api/utils/model_utils.py
+++ nlp_ml_api/utils/model_utils.py
@@ -29,22 +29,36 @@ def get_available_models():
     return list(MODELS.keys())
 
 
-def deploy_model(model: NLPModel, mode='localhost'):
-    assert mode in ['localhost', ]
+def deploy_model(model: NLPModel, mode:str = 'localhost', endpoint_name:str = None):
+    assert mode in ['localhost', 'heroku']
+
     # save model into endpoint folder
     output_path = 'endpoint/model_deploy.pickle'
     if os.path.exists(output_path):
         os.remove(output_path)
     save_model(model, output_path)
 
-    # call make
-    p = subprocess.call(['make','-C', 'endpoint', 'prod'])
     if mode == 'localhost':
-        try:
-            p = subprocess.check_output(['make', '-C', 'endpoint', 'up-prod'])
-            print(p)
-        except KeyboardInterrupt:
-            p = subprocess.call(['make', '-C', 'endpoint', 'down-prod'])
-        except Exception as e:
-            print(e)
-            p = subprocess.call(['make', '-C', 'endpoint', 'down-prod'])
+        # call make
+        p = subprocess.call(['make','-C', 'endpoint', 'prod'])
+        if mode == 'localhost':
+            try:
+                p = subprocess.check_output(['make', '-C', 'endpoint', 'up-prod'])
+                print(p)
+            except KeyboardInterrupt:
+                p = subprocess.call(['make', '-C', 'endpoint', 'down-prod'])
+            except Exception as e:
+                print(e)
+                p = subprocess.call(['make', '-C', 'endpoint', 'down-prod'])
+
+    elif mode == 'heroku':
+        assert endpoint_name
+        local_env = os.environ.copy()
+        local_env['HEROKU_APP_NAME'] = endpoint_name
+        # create the heroku app
+        p = subprocess.call(['make', '-C', 'endpoint', 'create_app'], env=local_env)
+        # deploy app
+        p = subprocess.call(['make', '-C', 'endpoint', 'deploy'], env=local_env)
+
+    else:
+        raise NotImplementedError
